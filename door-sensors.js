0be27a4ac0e529f1c1f7b1a38945bf6c21c489d7
paul <paulsmee.is@gmail.com>
2017-12-11 12:47:35 +1100

    Update

--- door-sensors.js
+++ door-sensors.js
@@ -1,11 +1,13 @@
+'use strict'
+
 var Gpio = require('pigpio').Gpio
 
 var doorstats = {}
 
 function doorOpen(whichDoor, doorState) {
-    var randomVar = doorState / 1023 * 5 
+    var randomVar = doorState 
     console.log(whichDoor + ' = ' + randomVar)
-	doorstats[whichDoor] = randomVar
+    doorstats[whichDoor] = randomVar
 }
 
 var door1 = new Gpio(4, {
@@ -19,54 +21,35 @@ door1.on('interrupt', function (level) {
 })
 
 
-// var door2 = new Gpio(5, {
-//     mode: Gpio.INPUT,
-//     pullUpDown: Gpio.PUD_DOWN,
-//     edge: Gpio.EITHER_EDGE
-//   })
-//
-// door2.on('interrupt', function (level) {
-//   if (level == 1) {
-//     console.log('Door 2 is open')
-//   } else {
-//     console.log('Door 2 is currently closed')
-//   }
-// })
-//
-// var door3 = new Gpio(6, {
-//     mode: Gpio.INPUT,
-//     pullUpDown: Gpio.PUD_DOWN,
-//     edge: Gpio.EITHER_EDGE
-//   })
-//
-// door3.on('interrupt', function (level) {
-//   if (level == 1) {
-//     console.log('Door 3 is open')
-//   } else {
-//     console.log('Door 3 is currently closed')
-//   }
-// })
-//
-// var door4 = new Gpio(7, {
-//     mode: Gpio.INPUT,
-//     pullUpDown: Gpio.PUD_DOWN,
-//     edge: Gpio.EITHER_EDGE
-//   })
-//
-//
-// door4.on('interrupt', function (level) {
-//   if (level == 1) {
-//     console.log('Door 4 is open')
-//   } else {
-//     console.log('Door 4 is currently closed')
-//   }
-// })
-//
-// console.log(door3)
-//
-//
-// doorstats.doorTwo = door2
-// doorstats.doorThree = door3
-// doorstats.doorFour = door4
+var door2 = new Gpio(5, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+  })
+
+door2.on('interrupt', function (level) {
+    doorOpen('door2', level)
+})
+
+var door3 = new Gpio(6, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+  })
+
+door3.on('interrupt', function (level) {
+    doorOpen('door3', level)
+})
+
+var door4 = new Gpio(7, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+  })
+
+
+door4.on('interrupt', function (level) {
+    doorOpen('door4', level)
+})
 
 module.exports = doorstats
