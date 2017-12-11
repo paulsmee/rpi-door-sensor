12ddd3f7b3bf37b10b8bbac2859b16e0587240d0
paulsmee <paulsmee.is@gmail.com>
2017-12-11 07:38:24 +0000

    Update

--- router.js
+++ router.js
@@ -10,20 +10,8 @@ router.get('/', function(req, res) {
 });
 
 // Need to add more routes once we know the functions that are exported from sensor.js
-router.get('/sensor/door/door1', function(req, res) {
-  res.send(doorstats.door1Status+'');
-})
-
-router.get('/sensor/door/door2', function(req, res) {
-  res.send(doorstats.door2Status+'');
-})
-
-router.get('/sensor/door/door3', function(req, res) {
-  res.send(doorstats.door3Status+'');
-})
-
-router.get('/sensor/door/door4', function(req, res) {
-  res.send(doorstats.door4Status+'');
+router.get('/sensor/doors', function(req, res, next) {
+  res.json(doorstats);
 })
 
 module.exports = router
